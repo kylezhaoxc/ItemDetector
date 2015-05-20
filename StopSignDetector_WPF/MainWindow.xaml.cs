5356747d41bf568347e284d2ef1368a379bec0a3
KyleZhao <kylezhaoxc@163.com>
2015-05-20 15:35:51 +0800

    I have set the layout of three images.Looking
    into image showing problems

---
+++ StopSignDetector_WPF/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Emgu.CV;
+using Emgu.Util;
+using Emgu.CV.ML;
+using Emgu.CV.CvEnum;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace StopSignDetector_WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    
+    public partial class MainWindow : Window
+    {
+        private Capture cap = new Capture(0);
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
