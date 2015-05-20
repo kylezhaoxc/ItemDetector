05c572c8bc9379326ca454bf42babc4842fbcd49
KyleZhao <kylezhaoxc@163.com>
2015-05-20 17:24:13 +0800

    most of the ui already set, able to take photos.

---
+++ StopSignDetector_WPF/CamHelper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Emgu.CV;
+using Emgu.Util;
+using Emgu.CV.Structure;
+using Emgu.CV.ML;
+using Emgu.CV.CvEnum;
+
+namespace StopSignDetector_WPF
+{
+    class CamHelper { 
+        private  static CamHelper helper;
+        private  Capture cap;
+        private int capcount = 0;
+        public static CamHelper Ret_Helper(ref Capture capture)
+        {
+            if (helper == null)
+            {
+                helper = new CamHelper();
+                helper.cap = capture;
+                return helper;
+            }
+            else return helper;
+        }
+
+        public void Ret_Frames(out Image<Bgr,Byte>small, out Image<Bgr, Byte> large)
+        {
+            small = null;large = null;
+            if (cap.Grab()) { large=cap.QueryFrame();small= cap.QuerySmallFrame();return; }
+        }
+      
+    }
+}
--- StopSignDetector_WPF/MainWindow.xaml.cs
+++ StopSignDetector_WPF/MainWindow.xaml.cs
@@ -2,10 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using Emgu.CV;
-using Emgu.Util;
-using Emgu.CV.ML;
-using Emgu.CV.CvEnum;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -16,19 +12,71 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
+
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.CV.Util;
+using Emgu.CV.Features2D;
+using System.IO;
+
 
 namespace StopSignDetector_WPF
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    
+
     public partial class MainWindow : Window
     {
-        private Capture cap = new Capture(0);
+        private Capture global_cap;
+        private CamHelper global_helper;
+        private Image<Bgr, Byte> video_small, video_large, model_small, model_large;
+        private bool model_selected = false;
         public MainWindow()
         {
             InitializeComponent();
+            global_cap = new Capture(0);
+            global_helper = CamHelper.Ret_Helper(ref global_cap);
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = new TimeSpan(40);
+            timer.Tick += new EventHandler(take_picture_and_analyze);
+            timer.Start();
+        }
+        public void take_picture_and_analyze(object sender, EventArgs e)
+        {
+            global_helper.Ret_Frames(out video_small,out video_large);
+            if (video_small != null)
+            {
+                show_Image(videoframe_s, video_small);
+            }
+        }
+        [System.Runtime.InteropServices.DllImport("gdi32")]
+        private static extern int DeleteObject(IntPtr o);
+        public static ImageSource ToBitmapSource(System.Drawing.Bitmap image)
+        {
+            IntPtr ptr = image.GetHbitmap();
+
+            ImageSource bs = System.Windows.Interop.Imaging.CreateBitmapSourceFromHBitmap(
+                ptr,
+                IntPtr.Zero,
+                Int32Rect.Empty,
+                System.Windows.Media.Imaging.BitmapSizeOptions.FromEmptyOptions());
+
+            DeleteObject(ptr);
+
+            return bs;
+        }
+        public void show_Image(System.Windows.Controls.Image imgbox, Image<Bgr, Byte> img)
+        {
+            imgbox.Source = ToBitmapSource(img.ToBitmap());
+        }
+        private void shot_Click(object sender, RoutedEventArgs e)
+        {
+            model_large = video_large;
+            model_small = video_small;
+            show_Image(modelpic_s, model_small);
+            if (model_large != null) model_selected = true;
         }
     }
 }
