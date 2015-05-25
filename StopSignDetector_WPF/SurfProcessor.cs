6bf0580a4afeb9f8e09e8cf137c2a04a74a0ab52
KyleZhao <kylezhaoxc@163.com>
2015-05-25 15:02:30 +0800

    losing weight

--- StopSignDetector_WPF/SurfProcessor.cs
+++ StopSignDetector_WPF/SurfProcessor.cs
@@ -11,10 +11,26 @@ using Emgu.CV.Util;
 
 namespace StopSignDetector_WPF
 {
+    class SpecificItemMatcher
+    {
+        public static void ApplySurfMatching(System.Windows.Controls.Image UI_TO_SHOW_THE_IMAGE, Image<Bgr, Byte> OBSERVED_IMAGE, ref SurfProcessor PROCESSOR_TO_USE, out long TIME_USED, out double MATCHED_AREA, int MIN_ACCEPTABLE_MATCHED_AREA, out System.Drawing.Point CENTER_OF_MATCHED_REGION)
+        {
+            Image<Gray, Byte> m_g = new Image<Gray, Byte>(AppDomain.CurrentDomain.BaseDirectory + "\\modelpicture.jpg");
+            System.Drawing.Bitmap v1 = OBSERVED_IMAGE.ToBitmap();
+            Image<Gray, Byte> v_g = new Image<Gray, Byte>(v1);
+            Image<Bgr, Byte> match_result = PROCESSOR_TO_USE.DrawResult(m_g, v_g, out TIME_USED, out MATCHED_AREA, MIN_ACCEPTABLE_MATCHED_AREA, out CENTER_OF_MATCHED_REGION);
+
+            UIHandler.show_Image(UI_TO_SHOW_THE_IMAGE, match_result);
+        }
+    }
+    interface MatchFilter_interface
+    {
+        int CountContours(System.Drawing.Bitmap temp);
+        double Getarea(PointF[] pts);
+    }
     class SurfProcessor : MatchFilter_interface
     {
-        int index = 0;
-        public  int CountContours(System.Drawing.Bitmap temp)
+        public int CountContours(System.Drawing.Bitmap temp)
         {
             int ContourNumber = 0;
             Image<Gray, Byte> gray = new Image<Gray, byte>(temp);
