da7590ef022d10a78cafbf3b1b03cd6e04435d61
KyleZhao <kylezhaoxc@163.com>
2015-05-29 13:51:18 +0800

    11

--- surrounding/Properties/Resources.Designer.cs
+++ surrounding/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace surrounding.Properties {
+namespace Surrounding.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace surrounding.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("surrounding.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Surrounding.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
--- surrounding/Properties/Settings.Designer.cs
+++ surrounding/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace surrounding.Properties {
+namespace Surrounding.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
